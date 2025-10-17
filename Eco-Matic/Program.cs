0e2a9a2fdabee690fbb4620742486da05de595f8
Sean <you@example.com>
2025-10-17 20:40:22 +0800

    chore: update Program.cs on dev

--- Eco-Matic/Program.cs
+++ Eco-Matic/Program.cs
@@ -1,2 +1,55 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading;
+
+// the project Eco-Matic!!
+class EcoMatic
+{
+
+}
+
+// parent guidelines for items in eco-matic
+abstract class VendingItem
+{
+    public string ItemName { get; set; }
+    public double ItemPrice { get; set; }
+
+    public VendingItem(string itemName, double itemPrice)
+    {
+        ItemName = itemName;
+        ItemPrice = itemPrice;
+    }
+
+    public abstract string GetDisplayInfo();
+    public abstract string GetExamineMessage();
+    public abstract string GetDispenseMessage();
+}
+
+// interfaces 
+interface IHasCalories
+{
+    public double Calories { get; set; }
+}
+
+interface IHasVolume
+{
+    public double Volume { get; set; }
+}
+
+class DrinkItem : VendingItem , IHasVolume 
+{
+    public double Volume { get; set; }
+
+    public DrinkItem(string itemName, double itemPrice, double volume) : base(itemName, itemPrice)
+    {
+        Volume = volume;
+    }
+
+    GetExamineInfo
+}
+
+class Program
+{
+
+}

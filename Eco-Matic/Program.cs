dd986e43e672e4f688e61c2ac7874cea42543fe1
Sean <you@example.com>
2025-10-17 23:29:27 +0800

    feat: implement default inventory and transaction log creation in Main method

--- Eco-Matic/Program.cs
+++ Eco-Matic/Program.cs
@@ -113,15 +113,63 @@ class Program
     {
         Console.WriteLine("Eco-Matic is still in development...");
 
-        bool flag = true;
+        string fp;
+        try
+        {
+            fp = "Inventory.csv";
+            if (!File.Exists(fp))
+            {
+                CreateDefaultInventory(fp);
+            }
+
+            fp = "TransactionLog.txt";
+            if (!File.Exists(fp))
+            {
+                CreateDefaultTransactionLog(fp);
+            }
+
+            
+        } 
+        catch (Exception ex)
+        {
+            Console.WriteLine("Oopsie, something went wrong... " + ex.Message); 
+        }
+
+        /* bool flag = true;
         while (flag)
         {
             MainMenu();
-        }
+        } */
     }
 
     public static void MainMenu()
     {
-        
+
+    }
+    
+    public static void CreateDefaultInventory(string fp)
+    {
+        Console.WriteLine("Creating Inventory.csv with default value...");
+        Thread.Sleep(1000);
+        using (StreamWriter writer = new StreamWriter(File.Create(fp)))
+        {
+            writer.WriteLine("Type,ItemName,ItemPrice,Additional");
+            writer.WriteLine("Snack,MrChips,30,160");
+            writer.WriteLine("Snack,Nova,50,140");
+            writer.WriteLine("Drink,Water,25,500");
+            writer.WriteLine("Drink,Coca-Cola,30,500");
+            writer.WriteLine("Misc,Pen,15,");
+            writer.WriteLine("Misc,Wet Wipes,30,");
+        }
+    }
+
+    public static void CreateDefaultTransactionLog(string fp)
+    {
+        Console.WriteLine("Creating TransactionLog.txt with default value...");
+        Thread.Sleep(1000);
+        using (StreamWriter writer = new StreamWriter(File.Create(fp)))
+        {
+            writer.WriteLine($"{DateTime.Now:yyyy-MM-dd HH:mm:ss} - EVENT: Transaction log initialized.");
+        }
     }
 }
